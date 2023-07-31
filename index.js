225de0072d6722656fa66df26416d908e44915b0
Longthuat1 <longthuat2409@gmail.com>
2023-07-31 20:53:36 +0700

    add BTJS1

---
+++ index.js
@@ -0,0 +1,63 @@
+// Bài 1 
+var salary, day, total ;
+salary = 100000;
+day = 30;
+total = salary * day;
+console.log('total : ',total);
+
+// Bài 2
+var a, b, c, d, e, average;
+a = 1;
+b = 2;
+c = 3;
+d = 4;
+e = 5;
+average = (a + b + c + d +e)/5;
+console.log('average : ',average);
+
+// Bài 3 
+var USD, VND, EX;
+VND = 23500 ;
+USD = 1 ;
+EX = 2 * USD * VND ;
+console.log('EX : ',EX);
+
+// Bài 4 
+
+var length , width, area, perimeter ;
+length = 20 ; 
+width = 10 ;
+area = length * width ;
+perimeter = ( length + width)*2
+console.log('area : ',area);
+console.log('perimeter : ',perimeter);
+
+// Bài 5 
+
+var n = 24 ;
+var unit,  ten, sum ;
+unit = n % 10 ;
+ten = Math.floor((n % 100)/10);
+sum = unit + ten ;
+console.log('unit : ',unit);
+console.log('ten : ',ten);
+console.log('sum : ',sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
